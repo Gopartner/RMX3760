0e5a314c13bde06be4ca22d5a6e142ae6e8155e2
arriRgb31 <febyyusuf12345@gmail.com>
2026-07-26 00:52:26 +0700

    Initial commit: TWRP Device Tree Realme C53 (RMX3760) Android 15

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_RMX3760.mk
+
+COMMON_LUNCH_CHOICES := \
+    RMX3760_arm64-user \
+    RMX3760_arm64-userdebug \
+    RMX3760_arm64-eng
