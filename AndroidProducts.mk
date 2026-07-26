028c8e340761bf0f1c7ecd5d4b141ed3b7fd3f47
arriRgb31 <febyyusuf12345@gmail.com>
2026-07-26 11:18:23 +0700

    Fix AndroidProducts.mk and product targets

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,7 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_RMX3760.mk
 
 COMMON_LUNCH_CHOICES := \
-    RMX3760_arm64-user \
-    RMX3760_arm64-userdebug \
-    RMX3760_arm64-eng
+    twrp_RMX3760-user \
+    twrp_RMX3760-userdebug \
+    twrp_RMX3760-eng
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,3 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 add_lunch_combo twrp_RMX3760-user
 add_lunch_combo twrp_RMX3760-userdebug
 add_lunch_combo twrp_RMX3760-eng
