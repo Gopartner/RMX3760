f39e953ea36c8c9c56ced15c97eaf97072737088
arriRgb31 <240433352+arriRgb31@users.noreply.github.com>
2026-08-02 23:59:35 +0700

    Switch to official repo tool and sync target to fox_14.1 stable tree

---
+++ twrp_RMX3760.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from RMX3760 device
+$(call inherit-product, device/realme/RMX3760/device.mk)
+
+PRODUCT_DEVICE := RMX3760
+PRODUCT_NAME := twrp_RMX3760
+PRODUCT_BRAND := realme
+PRODUCT_MODEL := RMX3760_full
+PRODUCT_MANUFACTURER := realme
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="RMX3760_full-user 15 AP3A.240905.015.A2 40 release-keys"
+
+BUILD_FINGERPRINT := realme/RMX3760/RE58C2:15/AP3A.240905.015.A2/T.R4T2.1777915050:user/release-keys
