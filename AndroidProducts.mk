393b98ff582c3ce115bf2434a65c24e1c38324c0
arriRgb31 <240433352+arriRgb31@users.noreply.github.com>
2026-08-05 13:29:53 +0700

    test

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,6 +6,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_RMX3760.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_RMX3760-trunk_staging-eng \
-    twrp_RMX3760-trunk_staging-userdebug \
-    twrp_RMX3760-trunk_staging-user
+    twrp_RMX3760-eng \
+    twrp_RMX3760-userdebug \
+    twrp_RMX3760-user
